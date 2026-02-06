edd54587c599761f7eb2fbff605012152712975d
verymoodyboi <m.mohiy179@gmail.com>
2026-02-07 01:50:09 +0300

    add privacy policy complance afety

--- yugen/src/components/SideMenu.tsx
+++ yugen/src/components/SideMenu.tsx
@@ -36,7 +36,8 @@ function SideMenu({
     <h2 className="text-2xl title text-emerald-50  mb-2">{children}</h2>
   );
   const navigationLinks: NavLink[] = [
-    { title: "Terms and Conditions", path: "/terms" },
+    { title: "About", path: "/about" },
+    { title: "Privacy Policy", path: "/privacy-policy" },
   ];
   const MenuButton = ({
     label,
