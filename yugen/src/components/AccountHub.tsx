f80c22ec652c5d466543afbd0e7dc258114c1e5c
verymoodyboi <m.mohoiy179@gmail.com>
2026-04-25 15:52:38 +0000

    before transcode v1

--- yugen/src/components/AccountHub.tsx
+++ yugen/src/components/AccountHub.tsx
@@ -97,7 +97,7 @@ function AccHub() {
               <SettingsIcon fontSize="small" />
               Settings
             </button>
-            {userInfo.username && (
+            {userInfo?.username && (
               <button
                 className="flex items-center gap-2 px-4 py-2 text-emerald-950  hover:bg-emerald-100 rounded-lg"
                 onClick={() => {
